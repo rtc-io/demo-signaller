30a9ee48fa52b2807013f49f041d363d32380b6e
Damon Oehlman <damon.oehlman@gmail.com>
2013-12-11 17:26:19 +1100

    Refactor out roles logic

--- public/js/common.js
+++ public/js/common.js
@@ -1,67 +1,45 @@
 /* jshint node: true */
 'use strict';
 
+var crel = require('crel');
+var moment = require('moment');
 var signaller = require('rtc-signaller');
 var socket = Primus.connect('http://localhost:1337/');
 var scope = signaller(socket);
+var friends = {};
 
-var me = 'sender1';
-var peer = 'sender2';
-var connectTime;
 var messageList = document.getElementById('messageList');
 var commandInput = document.getElementById('commandInput');
 
 socket.once('open', function() {
   // create a room
   console.log('announcing self');
-  connectTime = Date.now();
-  scope.announce({ role: me, room: 'test', time: connectTime });
+  scope.announce({ room: 'test' });
 });
 
 // catch announce message from partner
 scope.on('peer:announce', function(data) {
-
-  console.log('received announce : ' + JSON.stringify(data));
-  // when both announce themselves, but we have been later, re-announce ourselves
-  if (data.role === me) {
-    if (data.time > connectTime) {
-      me = 'sender2';
-      console.log('clashed roles, reannouncing as sender2.');
-      scope.announce({ role: me, room: 'test', time: connectTime });
-      console.log('connected');
-      writeChat("..connected", false);
-    }
-  } else {
-    peer = data.role;
-    console.log('connected');
-    writeChat("..connected", false);
-  }
-  return;
+  // we've got a new friend
+  friends[data.id] = data;
+  writeChat('new friend (id: ' + data.id + ') connected', data.id);
 });
 
-scope.on('peer:leave', function(data) {
-  console.log('received leave : ' + JSON.stringify(data));
-  writeChat("..peer disconnected", false);
-  peer = me;
-
+scope.on('peer:leave', function(id) {
+  if (friends[id]) {
+    writeChat('friend ' + id + ' left', id);
+    friends[id] = undefined;
+  }
 });
 
-function writeChat(text, local) {
-  var row = document.createElement("tr");
-  var date = new Date;
-  var hour = date.getHours();
-  var minutes = date.getMinutes();
-  (minutes < 10) && (minutes = '0' + minutes);
+function writeChat(text, id) {
+  messageList.appendChild(crel('tr', {
+      class: id === scope.id ? 'local' : 'remote',
+      'data-sender': id
+    },
+    crel('td', moment().format('MM:SS')),
+    crel('td', text)
+  ));
 
-  if (me === 'teacher' && local || me === 'student' && !local) {
-    row.innerHTML += "<td>" + hour + ":" + minutes + "</td>";
-    row.innerHTML += "<td style='text-color:red'>" + text + "</td>";
-  } else {
-    row.innerHTML += "<td>" + hour + ":" + minutes + "</td>";
-    row.innerHTML += "<td style='text-color:green'>" + text + "</td>";
-  }
-
-  messageList.appendChild(row);
   messageList.scrollTop = messageList.scrollHeight;
 }
 
@@ -69,22 +47,18 @@ function writeChat(text, local) {
 function handleCommand(evt) {
   if (evt && evt.keyCode === 13 && commandInput.value != '') {
     scope.send('/chat', {
-      room: 'test',
-      text: commandInput.value
+      text: commandInput.value,
+      sender: scope.id
     });
 
-    writeChat(commandInput.value, true);
+    writeChat(commandInput.value, scope.id);
     commandInput.value = '';
   }
 }
+
 commandInput.addEventListener('keydown', handleCommand);
 
 // handle receiving input string
 scope.on('chat', function(srcState, data) {
-  // if the chat message is not for this room ignore
-  if (data.room !== 'test') {
-    return;
-  }
-
-  writeChat(data.text, false);
+  writeChat(data.text, data.sender);
 });
